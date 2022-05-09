341e959a4141ea2b3ed6137cd618adcadfce4a30
Derek Lui <luiderek@outlook.com>
2022-05-09 16:18:19 +0000

    creating an object to represent the XML data

--- player.js
+++ player.js
@@ -1,8 +1,9 @@
 let parser = new DOMParser();
 
 let xhr = new XMLHttpRequest();
-let xmlArr;
-let extrack;
+// let xmlArr;
+// let extrack;
+let trackObject;
 
 xhr.open('GET', '/mellow.xml');
 xhr.onreadystatechange = function () {
@@ -13,10 +14,10 @@ xhr.onreadystatechange = function () {
       xmlParsed = parser.parseFromString(xmlMellow, "text/xml");
 
       let tracklist = xmlParsed.children[0].children[0].children;
-      for(let track of tracklist){
-        trackToObject(track)
-        appendTrackToList(track.children);
-        extrack = track.children;
+      trackObject = tracklistObject(tracklist);
+
+      for(let track of trackObject){
+        appendTrackToList(track);
       }
     } else {
       console.error('There was a mistake with XHR of the track list');
@@ -27,15 +28,29 @@ xhr.send();
 
 const $trackList = document.querySelector('.track-list');
 
-function trackToObject(trackHTMLCollection) {
-  result = [];
+function tracklistObject(trackHTMLCollection) {
+  let result = [];
+  let next_id = 0;
+  for (let track of trackHTMLCollection) {
+    result.push({
+      author: track.children[0].textContent,
+      name: track.children[1].textContent,
+      link: track.children[2].textContent,
+      id: ++next_id
+    })
+  }
   return result;
 }
 
-function appendTrackToList(trackHTMLCollection){
+function appendTrackToList(track){
   let $div = document.createElement('div');
-  $div.className = 'track';
-  $div.textContent = trackHTMLCollection[1].textContent;
+  $div.className = `track t-${track.id}`;
+  $div.textContent = track.name;
   // [0] author [1] trackname [2] resource link
   $trackList.appendChild($div);
 }
+
+$trackList.addEventListener('click', function (event) {
+  let selected_id = event.target.classList[1].split('-')[1];
+  console.log(trackObject[selected_id-1]);
+});
